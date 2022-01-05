fd78f06a4b2331275b152113231c6568d44c0ac9
vanhuong06 <nguyenvanhuong009@gmail.com>
2022-01-05 23:43:35 +0700

    lam lai trang details

--- client/src/components/detailsModal/Details.jsx
+++ client/src/components/detailsModal/Details.jsx
@@ -1,18 +1,20 @@
 import './details.scss';
 import { useRef } from 'react';
 import { staggerOne, modalOverlayVariants, modalVariants, modalFadeInUpVariants } from "../../motionUtils";
-// import { selectModalContent, selectModalState } from "../../redux/modal/modal.selectors";
+import { hideModalDetail } from "../../redux/modal/modal.actions";
+import { useDispatch, useSelector } from "react-redux";
+import { selectModalContent, selectModalState } from "../../redux/modal/modal.selectors";
 import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { VscChromeClose } from "react-icons/vsc";
 import { FaMinus, FaPlay, FaPlus } from "react-icons/fa";
-
+import useOutsideClick from "../../hooks/useOutSideClick";
 
 const DetailModal = () => {
-	// const dispatch = useDispatch();
-	// const modalClosed = useSelector(selectModalState);
-	// const modalContent = useSelector(selectModalContent);
-	// const handleModalClose = () => dispatch(hideModalDetail());
+	const dispatch = useDispatch();
+	const modalClosed = useSelector(selectModalState);
+	const modalContent = useSelector(selectModalContent);
+	const handleModalClose = () => dispatch(hideModalDetail());
 	// const { title, desc, genre} = modalContent;
 	// const joinedGenres = genresConverted ? genresConverted.join(', ') : "Not available";
 	// const maturityRating = adult === undefined ? "Not available" : adult ? "Suitable for adults only" : "Suitable for all ages";
@@ -20,26 +22,14 @@ const DetailModal = () => {
 	const modalRef = useRef();
 
 
-	// const handleAdd = (event) => {
-	// 	event.stopPropagation();
-	// 	dispatch(addToFavourites({ ...modalContent, isFavourite }));
-	// }
-	// const handleRemove = (event) => {
-	// 	event.stopPropagation();
-	// 	dispatch(removeFromFavourites({ ...modalContent, isFavourite }));
-	// 	if (!modalClosed) handleModalClose();
-	// }
-	// const handlePlayAnimation = event => {
-	// 	event.stopPropagation();
-	// 	handleModalClose();
-	// };
-	// useOutsideClick(modalRef, () => {
-	// 	if (!modalClosed) handleModalClose();
-	// });
+
+	useOutsideClick(modalRef, () => {
+		if (!modalClosed) handleModalClose();
+	});
 
 	return (
 		<AnimatePresence exitBeforeEnter>
-		
+		{!modalClosed && (
 				<>
 					<motion.div
 						variants={modalOverlayVariants}
@@ -47,18 +37,18 @@ const DetailModal = () => {
 						animate="visible"
 						exit="hidden"
 						key="modalOverlay"
-						className={`Modal__overlay `}
+						className={`Modal__overlay ${modalClosed && 'Modal__invisible'}`}
 					>
 						<motion.div
 							key="modal"
 							variants={modalVariants}
 							ref={modalRef}
-							className={`Modal__wrp
+							className={`Modal__wrp ${modalClosed && 'Modal__invisible'}
                              `}
 						>
 							<motion.button
 								className="Modal__closebtn"
-								// onClick={handleModalClose}
+								onClick={handleModalClose}
 							>
 								<VscChromeClose />
 							</motion.button>
@@ -66,61 +56,40 @@ const DetailModal = () => {
 								<div className="Modal__image--shadow" />
 								<img
 									className="Modal__image--img"
-									src="https://kenh14cdn.com/thumb_w/660/203336854389633024/2021/12/16/800500-1-1639669631931545455159.jpg"
+									src={modalContent.imgSm}
 									alt="img "
 								/>
 								<div className="Modal__image--buttonswrp">
 									<Link
 										className="Modal__image--button"
-										// onClick={handlePlayAnimation}
-										to={'/play'}
+										to={{ pathname: "/watch", movie: modalContent }}
 									>
 										<FaPlay />
 										<span>Play</span>
 									</Link>
-									{/* {!isFavourite
-										? (
-											<button className='Modal__image--button-circular' onClick={handleAdd}>
-												<FaPlus />
-											</button>
-										): (
-											<button className='Modal__image--button-circular' onClick={handleRemove}>
-												<FaMinus />
-											</button>
-										)} */}
 								</div>
 							</div>
 							<motion.div variants={staggerOne} initial="initial" animate="animate" exit="exit" className="Modal__info--wrp">
-								<motion.h3 variants={modalFadeInUpVariants} className="Modal__info--title">title</motion.h3>
-								<motion.p variants={modalFadeInUpVariants} className="Modal__info--description">desc</motion.p>
+								<motion.h3 variants={modalFadeInUpVariants} className="Modal__info--title">{modalContent.title}</motion.h3>
+								<motion.p variants={modalFadeInUpVariants} className="Modal__info--description">{modalContent.desc}</motion.p>
 								<motion.hr variants={modalFadeInUpVariants} className="Modal__info--line"/>
-								<motion.h4 variants={modalFadeInUpVariants} className="Modal__info--otherTitle">Info on <b>title</b></motion.h4>
+								<motion.h4 variants={modalFadeInUpVariants} className="Modal__info--otherTitle">Info on <b>{modalContent.title}</b></motion.h4>
 								<motion.div variants={modalFadeInUpVariants} className="Modal__info--row">
-									<span className='Modal__info--row-label'>Genres: </span>
-									{/* <span className="Modal__info--row-description">{joinedGenres}</span> */}
+									<span className='Modal__info--row-label'>Genres: {modalContent.genre}</span>
 								</motion.div>
 								<motion.div variants={modalFadeInUpVariants} className="Modal__info--row">
 									<span className='Modal__info--row-label'>
-										{/* {release_date ? "Release date: " : "First air date: "} */}
+										Year: {modalContent.year}
 									</span>
-									{/* <span className="Modal__info--row-description">{reducedDate}</span> */}
 								</motion.div>
 								<motion.div variants={modalFadeInUpVariants} className="Modal__info--row">
-									<span className='Modal__info--row-label'>Average vote: </span>
-									{/* <span className="Modal__info--row-description">{vote_average || "Not available"}</span> */}
-								</motion.div>
-								<motion.div variants={modalFadeInUpVariants} className="Modal__info--row">
-									<span className='Modal__info--row-label'>Original language: </span>
-									{/* <span className="Modal__info--row-description">{capitalizeFirstLetter(original_language)}</span> */}
-								</motion.div>
-								<motion.div variants={modalFadeInUpVariants} className="Modal__info--row">
-									<span className='Modal__info--row-label'>Age classification: </span>
-									{/* <span className="Modal__info--row-description">{maturityRating}</span> */}
+									<span className='Modal__info--row-label'>{modalContent.limit}</span>
 								</motion.div>
 							</motion.div>
 						</motion.div>
 					</motion.div>
 				</>
+		)}
 		</AnimatePresence>
 	)
 }
