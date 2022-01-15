8312ac334dc75e5b0367f2998a33eaacc91362c1
vanhuong06 <nguyenvanhuong009@gmail.com>
2022-01-15 13:54:25 +0700

    sua lai pagewatch

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -14,66 +14,51 @@ import {
   CurrentTimeDisplay,
   TimeDivider,
   PlaybackRateMenuButton,
-  VolumeMenuButton
-} from 'video-react';
-import 'video-react/dist/video-react.css';
+  VolumeMenuButton,
+} from "video-react";
+import "video-react/dist/video-react.css";
 
 export default function Watch() {
   const location = useLocation();
   const movie = location.movie;
   const history = useHistory();
   const [movies, setMovies] = useState({});
-  const [list, setList] = useState([]);
-  const vid = document.getElementById("myVideo");
-  useEffect(() => {
-    const getMovie = async () => {
-      try {
-        const res = await axios.get("/movie/find/" + movie._id, {
-          headers: {
-            token:
-              "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
-          },
-        });
-        setMovies(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getMovie();
-  }, []);
-
-  useEffect(() => {
-    const currentMovie = JSON.parse(localStorage.getItem("itemTime"));
-    if (currentMovie) {
-      setList(currentMovie);
-    }
-  }, []);
-  // const prevNext = () => {
-  //   vid.currentTime -= 10;
-  // };
-  // const skipNext = () => {
-  //   vid.currentTime += 10;
-  // };
-
+  // useEffect(() => {
+  //   const getMovie = async () => {
+  //     try {
+  //       const res = await axios.get("/movie/find/" + movie._id, {
+  //         headers: {
+  //           token:
+  //             "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+  //         },
+  //       });
+  //       setMovies(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   getMovie();
+  // }, []);
+  const sources = { video: movie.video };
   return (
     <div className="watch">
-       <button onClick={history.goBack}>
+      <button onClick={history.goBack}>
         <div className="back">
           <ArrowBackOutlined />
           Back
         </div>
       </button>
-      <Player poster={movies.imgSm}>
-      <source src={movie.video} />
-      <ControlBar>
-        <ReplayControl seconds={10} order={1.1} />
-        <ForwardControl seconds={30} order={1.2} />
-        <CurrentTimeDisplay order={4.1} />
-        <TimeDivider order={4.2} />
-        <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-        <VolumeMenuButton disabled />
-      </ControlBar>
-    </Player>
+      <Player poster={movie.imgSm} autoPlay>
+        <source src={movie.video} />
+        <ControlBar>
+          <ReplayControl seconds={10} order={1.1} />
+          <ForwardControl seconds={30} order={1.2} />
+          <CurrentTimeDisplay order={4.1} />
+          <TimeDivider order={4.2} />
+          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+          <VolumeMenuButton disabled />
+        </ControlBar>
+      </Player>
     </div>
   );
 }
